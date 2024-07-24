7c4eadb4fe9a0e19dedad41dc0b11a28bdaad438
sara22mustafa <103318523+sara22mustafa@users.noreply.github.com>
2024-07-24 21:17:28 +0300

    Update fetchBreedList.js

--- src/fetchBreedList.js
+++ src/fetchBreedList.js
@@ -1,7 +1,7 @@
 export default async function fetchBreedList({queryKey}) {
   const animal = queryKey[1];
   if (!animal) return [];
-  const res = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
+  const res = await fetch(`https://pets-v2.dev-apis.com/breeds?animal=${animal}`);
 
   if (!res.ok) {
     throw new Error(`details/${animal} not found`);
