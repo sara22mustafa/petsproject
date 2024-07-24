9d69cf71b14e70e2c18780e9fd18c1a6d8b32baa
sara22mustafa <103318523+sara22mustafa@users.noreply.github.com>
2024-07-24 21:17:47 +0300

    Update fetchPetDetails.js

--- src/fetchPetDetails.js
+++ src/fetchPetDetails.js
@@ -1,6 +1,6 @@
 export default async function fetchPetDetails({ queryKey }) {
   const id = queryKey[1];
-  const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
+  const res = await fetch(`https://pets-v2.dev-apis.com/pets?id=${id}`);
 
   if (!res.ok) {
     throw new Error(`details/${id} not found`);
